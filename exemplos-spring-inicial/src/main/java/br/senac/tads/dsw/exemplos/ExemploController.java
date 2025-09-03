474dcf7c2692fbd72351cc71d5dae28f68573f88
ftsuda-senac <ftsuda.senac@gmail.com>
2025-09-02 22:15:32 -0300

    Versão final RestController

--- exemplos-spring-inicial/src/main/java/br/senac/tads/dsw/exemplos/ExemploController.java
+++ exemplos-spring-inicial/src/main/java/br/senac/tads/dsw/exemplos/ExemploController.java
@@ -2,38 +2,21 @@ package br.senac.tads.dsw.exemplos;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class ExemploController {
 
-    private ObjectMapper objectMapper;
-
-    public ExemploController(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @GetMapping
-    // @ResponseBody
-    public String gerarJson(
+    public Dados gerarJson(
             @RequestParam("nome") String nome,
             @RequestParam("email") String email,
             @RequestParam("time") String time) {
        
-        Dados dados = new Dados(nome, email, time, "Ribeirao Preto",
+        return new Dados(nome, email, time, "Santos",
         LocalDateTime.now());
-        try {
-            return objectMapper.writeValueAsString(dados);
-        } catch (Exception ex) {
-            return  "{ }";
-        }
     }
 
 }
