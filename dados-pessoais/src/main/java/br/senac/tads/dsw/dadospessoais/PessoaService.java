749f58b9850590c8e352c5117227e0cfcb849c41
ftsuda-senac <ftsuda.senac@gmail.com>
2025-09-09 22:41:00 -0300

    Dto e Service

---
+++ dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/PessoaService.java
@@ -0,0 +1,50 @@
+package br.senac.tads.dsw.dadospessoais;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PessoaService {
+
+    private Map<String, Pessoa> mapPessoas;
+
+    public PessoaService() {
+        mapPessoas = new HashMap<>();
+        mapPessoas.put("fulano", new Pessoa("fulano", "Fulano da Silva",
+        LocalDate.parse("2000-10-20"), 
+        "fulano@email.com", "11 91234-1234", "senhaAbcd1234",
+        List.of("Java", "Spring Boot")));
+        mapPessoas.put("ciclano", new Pessoa("ciclano", "Ciclano de Souza",
+        LocalDate.parse("1999-05-14"), 
+        "ciclano@email.com", "11 98765-8765", "senhaAbcd1234",
+        List.of("HTML", "CSS", "Javascript")));
+        mapPessoas.put("beltrana", new Pessoa("beltrana", "Beltrana dos Santos",
+        LocalDate.parse("2021-02-18"), 
+        "beltrana@email.com", "11 91122-1234", "senhaAbcd1234",
+        List.of("Javascript", "Angular", "React")));
+    }
+
+    public List<Pessoa> findAll() {
+        List<Pessoa> pessoas = new ArrayList<>(mapPessoas.values());
+        return pessoas;
+    }
+
+    public Pessoa findByUsername(String username) {
+        return null;
+    }
+
+    public Pessoa addNew(Pessoa pessoa) {
+        return null;
+    }
+
+    public Pessoa update(String username, Pessoa pessoa) {
+        return null;
+    }
+
+    public void delete(String username) {
+
+    }
+
+}
