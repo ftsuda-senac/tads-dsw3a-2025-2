355464ce8031d6663d2d7ae66a7009ae867c8dfe
ftsuda-senac <ftsuda.senac@gmail.com>
2025-09-16 21:48:55 -0300

    Exemplos Header e Exception para 404

--- dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/PessoaRestController.java
+++ dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/PessoaRestController.java
@@ -1,13 +1,17 @@
 package br.senac.tads.dsw.dadospessoais;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class PessoaRestController {
@@ -20,6 +24,7 @@ public class PessoaRestController {
         return service.findAll();
     }
 
+    // Equivalencia da annotation (forma reduzida X forma completa)
     // @RequestParam("username") == @RequestParam(value = "username")
 
     @GetMapping("/apelido")
@@ -30,7 +35,29 @@ public class PessoaRestController {
 
     @GetMapping("/{username}")
     public Pessoa findByUsername(@PathVariable("username") String u) {
-        return service.findByUsername(u);
+        Pessoa resultado = service.findByUsername(u);
+        if (resultado == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Pessoa %s não encontrada".formatted(u));
+        }
+        return resultado;
     }
+
+    @GetMapping("/tudo/{username}")
+    public Map<String, Object> findExemploTudo(
+        @PathVariable("username") String u,
+        @RequestParam(value = "extra1", defaultValue = "xpto") String extra1,
+        @RequestParam(value = "extra2", required =  false) String extra2,
+        @RequestHeader("user-agent") String userAgent) {
+
+        Map<String, Object> mapResposta = new HashMap<>();
+        mapResposta.put("usuario", service.findByUsername(u));
+        mapResposta.put("extra1", extra1);
+        mapResposta.put("extra2", extra2);
+        mapResposta.put("user-agent", userAgent);
+        return mapResposta;
+
+    }
+
 
 }
